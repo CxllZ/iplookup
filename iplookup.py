9766c20a4398f056975b6375a4d2a648cb772eb1
CxllZ <72345758+CxllZ@users.noreply.github.com>
2021-03-15 09:47:25 +0000

    Update iplookup.py

--- iplookup.py
+++ iplookup.py
@@ -1,12 +1,12 @@
-import urllib.request as urllib2
-import json
-import pprint
+import urllib.request as urllib2, json, pprint
 
 ip = input("Enter Ip --> : ")
-url = "http://ip-api.com/json/"+ip+"?fields=country,countryCode,regionName,city,zip,lat,lon,timezone,isp,org,as,proxy,hosting,query"
+url = "http://ip-api.com/json/"+ip+"?fields=status,message,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,offset,currency,isp,org,as,asname,reverse,mobile,proxy,hosting,query"
 
 response = urllib2.urlopen(url)
 data = response.read()
 values = json.loads(data)
 
+print("Not Always Accurate!")
 pprint.pprint(values)
+print("Not Always Accurate!")
